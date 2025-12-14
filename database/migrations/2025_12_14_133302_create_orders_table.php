45550073da0fd3fb6a84507e7186b3e0d9aabc61
Firdan <firdana.id@gmail.com>
2025-12-14 20:38:34 +0700

    Inisialisasi dan migrasi

---
+++ database/migrations/2025_12_14_133302_create_orders_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained('users')->cascadeOnDelete();
+            $table->foreignId('payment_type_id')->constrained('payments')->cascadeOnDelete();
+            $table->string('name');
+            $table->integer('total_price');
+            $table->integer('total_paid');
+            $table->integer('total_return');
+            $table->string('receipt_code');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('orders');
+    }
+};
